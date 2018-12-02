ccfd448f6c8dca9b3db331e44da7d82f43c88964
Brandon Choi <choi.brandon.h@gmail.com>
2018-12-02 17:15:28 -0500

    catchup

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyMusicBar()};
 
 var header = document.getElementById("musicPlayer");
 var sticky = header.offsetTop;
 
-function myFunction() {
+function stickyMusicBar() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
+
+//pseudocode for music bar
+//function showMusicBar() {
+//  if clicked {
+//    hide "LISTEN?";  
+//    classList.add("shownMusicBar");
+//    start playing the music;
+//    }
+//}
