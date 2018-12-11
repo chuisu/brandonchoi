5ce7cae4f9d1fb93e11d58364026a8c17ab55ba3
chuisu <choi.brandon.h@gmail.com>
2018-12-11 15:29:31 -0800

    Media player tracking/seeking

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ var header = document.getElementById("musicPlayer");
 var sticky = header.offsetTop;
 var aud = document.getElementById("musicPlayer_audio"); 
 var playpauseButton = document.getElementById("musicPlayer_playpauseButton");
+var timeline = document.getElementById("musicPlayer_timeline");
+var playhead = document.getElementById("musicPlayer_playhead");
 
 window.onscroll = function() {stickyMusicBar()};
 playpauseButton.onclick = function() {togglePlay()};
-aud.ontimeupdate = function() {progressPlayhead};
+aud.ontimeupdate = function() {progressPlayhead()};
+timeline.onclick = function() {}; // get the x coordinate and move the playhead there
+playhead.onclick = function() {}; // maybe we don't need this? see below
+playhead.onmousedown = function (){}; // start updating where the playhead is based on the mouse's x coordinates
+playhead.onmouseup = function() {}; // release the playhead on the last known x coordinate and update the track's position
 
 function stickyMusicBar() {
   if (window.pageYOffset > sticky) {
@@ -28,7 +34,9 @@ function togglePlay() {
 }
 
 function progressPlayhead() {
-
+  var playPercent = 100 * (aud.currentTime / aud.duration);
+  playhead.style.marginLeft = playPercent + "%";
+//  console.log("we tried to move the playhead to " + playPercent + "%");
 }
 
 $("#musicPlayer").click(function() {
@@ -37,6 +45,8 @@ $("#musicPlayer").click(function() {
     aud.play();
     playpauseButton.classList.add("fa");
     playpauseButton.classList.add("fa-pause");
+    timeline.classList.add("musicPlayer_timeline");
+    playhead.classList.add("musicPlayer_playhead");
   }
 });
 
